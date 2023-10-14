b633581120bd954671f00b66f4af9336bd6843c0
PareshD <pareshparmar232@gmail.com>
2023-10-14 11:43:29 +1100

    Changes made

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -20,20 +20,31 @@
 
 <section class="content">
     <div class="container-fluid">
+        <div class="row mb-3">
+            <div class="col-md-12">
+                <a class="btn btn-primary" type="button" href="{{ route('user.create') }}">
+                    Add New User
+                </a>
+            </div>
+        </div>
         <div class="card">
             <div class="card-header bg-primary">Users</div>
             <div class="card-body">
-                <table class="table" id="user-table" width="100%">
-                    <thead>
-                        <tr>
-                            <td>Name</td>
-                            <td>Phone</td>
-                            <td>Stores</td>
-                            <td>Actions</td>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table" id="user-table" width="100%">
+                            <thead>
+                                <tr>
+                                    <td>Name</td>
+                                    <td>Phone</td>
+                                    <td>Stores</td>
+                                    <td>Actions</td>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div><!-- /.container-fluid -->
