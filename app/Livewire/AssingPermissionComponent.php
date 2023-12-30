f007391aa299a7c8ae99ef9a695b90cad6cd9463
PareshD <pareshparmar232@gmail.com>
2023-12-30 20:27:48 +1100

    Checkbox added

--- app/Livewire/AssingPermissionComponent.php
+++ app/Livewire/AssingPermissionComponent.php
@@ -4,7 +4,6 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Spatie\Permission\Models\Permission;
-use Livewire\Attributes\Computed;
 
 
 class AssingPermissionComponent extends Component
@@ -12,21 +11,33 @@ class AssingPermissionComponent extends Component
     public $role;
     public $roleHasPermissions = [];
 
-    #[Computed]
-    public function permissions() {
-        return Permission::select('name', 'module', 'description')->get()->groupBy('module');
-    }
+    public $permissions;
+    public $state = [];
     public function mount() {
 
         $this->role->getAllPermissions()->filter(function($data){
             array_push($this->roleHasPermissions, $data->name);
         });
+        $this->permissions = Permission::select('name', 'module', 'description')->get()->groupBy('module')->toArray();
+
+        $this->buildPermissionState();
     }
     public function render()
     {
         return view('livewire.assing-permission-component');
     }
 
+    public function buildPermissionState() {
+        foreach ($this->permissions as $permissionModules) {
+            foreach ($permissionModules as $permission) {
+                if (in_array($permission['name'], $this->roleHasPermissions)) {
+                    $this->state[$permission['name']] = true;
+                } else {
+                    $this->state[$permission['name']] = false;
+                }
+            }
+        }
+    }
     public function saveData() {
 
     }
--- resources/views/livewire/assing-permission-component.blade.php
+++ resources/views/livewire/assing-permission-component.blade.php
@@ -1,11 +1,26 @@
 <div>
-    <table class="table">
+    <table class="table ">
         <tr>
-            <td></td>
             <td>Permission Module</td>
+            <td></td>
             <td>Permission Name</td>
             <td>Permission Description</td>
         </tr>
-        @dd($this->permissions);
+        @forelse ($this->permissions as $permissionModule)
+            @forelse ($permissionModule as $permission)
+            <tr class="form-group">
+                    @if($loop->first)
+                    <td rowspan="{{ $loop->count }}">{{ $permission['module'] }}</td>
+                    @endif
+                    <td>
+                        <input type="checkbox" class="form-control" wire:model='{{ 'state.'.$permission['name'] }}'>
+                    </td>
+                    <td>{{ $permission['name'] }}</td>
+                    <td>{{ $permission['description'] }}</td>
+                </tr>
+                @empty
+            @endforelse
+            @empty
+        @endforelse
     </table>
 </div>
