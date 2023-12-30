625c7fd91875ad280f41ccd7bebc5579eac6828e
PareshD <pareshparmar232@gmail.com>
2023-12-31 09:50:24 +1100

    Migrations and models added

---
+++ app/Http/Controllers/CashDropController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCashDropRequest;
+use App\Http\Requests\UpdateCashDropRequest;
+use App\Models\CashDrop;
+
+class CashDropController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCashDropRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CashDrop $cashDrop)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CashDrop $cashDrop)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCashDropRequest $request, CashDrop $cashDrop)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CashDrop $cashDrop)
+    {
+        //
+    }
+}
